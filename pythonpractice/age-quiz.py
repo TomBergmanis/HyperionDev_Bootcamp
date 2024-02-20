c8f7be9d16410a72fe9890505acfbda12672aa09
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2024-02-20 18:53:59 +0000

    added error handling to age-quiz

--- pythonpractice/age-quiz.py
+++ pythonpractice/age-quiz.py
@@ -5,30 +5,33 @@
     if the user is 21 output "Congrats on your 21st!"
     if any other gae output "Age is but a number" """
 
-age = int(input("Please enter your age here: "))
- # below is first attempt which causes the if age >= 40 print statement to occur during the age >= 65 statement as well
-""" if age >= 40:
-    print("You're over the hill.")
-elif age > 100:
-    print("Sorry, you're dead.")
-elif age >= 65:
-    print("Enjoy your retirement!")
-elif age < 13:
-    print("You qualify for the kiddie discount.")
-elif age == 21:
-    print("Congrats on your 21st!")
-else:
-    print("Age is but a number.") """
-# reordered to the below, the if, elif, else statement should work as intended 
-if age < 13:
-    print("You qualify for the kiddie discount.")
-elif age == 21:
-    print("Congrats on your 21st!")
-elif age > 100:
-    print("Sorry, you're dead.")
-elif age >= 65:
-    print("Enjoy your retirement!")
-elif age >= 40:
-    print("You're over the hill.")
-else:
-    print("Age is but a number.") 
+try: 
+    age = int(input("Please enter your age here: "))
+    # below is first attempt which causes the if age >= 40 print statement to occur during the age >= 65 statement as well
+    """ if age >= 40:
+        print("You're over the hill.")
+    elif age > 100:
+        print("Sorry, you're dead.")
+    elif age >= 65:
+        print("Enjoy your retirement!")
+    elif age < 13:
+        print("You qualify for the kiddie discount.")
+    elif age == 21:
+        print("Congrats on your 21st!")
+    else:
+        print("Age is but a number.") """
+    # reordered to the below, the if, elif, else statement should work as intended 
+    if age < 13:
+        print("You qualify for the kiddie discount.")
+    elif age == 21:
+        print("Congrats on your 21st!")
+    elif age > 100:
+        print("Sorry, you're dead.")
+    elif age >= 65:
+        print("Enjoy your retirement!")
+    elif age >= 40:
+        print("You're over the hill.")
+    else:
+        print("Age is but a number.") 
+except ValueError:
+    print("Invalid input. Please enter a valid age: (Hint: Enter a number)")
