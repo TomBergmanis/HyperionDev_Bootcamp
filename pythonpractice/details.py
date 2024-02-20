f989e7eb59fceae75450b60cc8afe783238c0f59
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2024-02-20 18:43:13 +0000

    Update details.py

--- pythonpractice/details.py
+++ pythonpractice/details.py
@@ -8,4 +8,7 @@ user_age = input("Enter your age here: ")
 user_house_number = input("Enter your house number: ")
 user_street_name = input("Enter your street name: ")
 
+# Using an f string
+print(f"This is ${user_name}. He is ${user_age} years old and lives at number ${user_house_number} on ${user_street_name}.")
+# Using .format 
 print("This is {}. He is {} years old and lives at house number {} on {}".format(user_name, user_age, user_house_number, user_street_name))
