7cdf0d24ae9eaef867f9d5565c452f374f90508f
Thomas Bergmanis <60963045+TomBergmanis@users.noreply.github.com>
2024-02-20 19:02:37 +0000

    updating award.py file to include error handling

--- pythonpractice/award.py
+++ pythonpractice/award.py
@@ -7,20 +7,24 @@
     if the particpant is greater than or equal to 111 mins they win nothing.
 """
 # variables created to store the times for each event. 
-swim_time = int(50) # example times have been put in to test the statement below
-cycle_time = int(50)
-run_time = int(6)
+try: 
+    swim_time = int(input("Enter your swim time: ")) 
+    cycle_time = int(input("Enter your cycle time: "))
+    run_time = int(input("Enter your run time: "))
 
-total_time = swim_time + cycle_time + run_time # a variable which sums the individual times of each event
+    total_time = swim_time + cycle_time + run_time # A variable which sums the individual times of each event
 
-print(total_time) # testing to see if the total time variable sums the total time of each part ofthe triathlon
+    print(total_time) # Testing to see if the total time variable sums the total time of each part ofthe triathlon
 
-# using an if elif else statement to determine the award the person has. 
-if total_time <= 100: # a time of 100 munutes or below 
+except ValueError:
+    print("Invalid input. Please enter a number. ")
+
+# Using an if elif else statement to determine the award the person has. 
+if total_time <= 100: # A time of 100 munutes or below 
     print("Well done! Provincial Colours")
-elif 101 >= total_time <= 105: #  a time of 101 or above and 105 munutes or below 
+elif 101 <= total_time <= 105: #  A time of 101 or above and 105 minutes or below 
     print("well done as well! Provincial Half Colours")
-elif 106 >= total_time <= 110: #  a time of 106 or above and 110 munutes or below 
+elif 106 <= total_time <= 110: #  A time of 106 or above and 110 munutes or below 
     print("Not bad! Provincial Scroll")
 else: 
-    print("Sorry better luck next time") # this should catch all other options or anything over 110 minutes 
+    print("Sorry better luck next time") # This should catch all other options or anything over 110 minutes 
